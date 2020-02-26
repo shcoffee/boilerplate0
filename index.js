5075c4da534c45368eb64522bc6e16d633f9247e
shcoffee <sheepbrandy@gmail.com>
2020-02-26 11:56:27 +0900

    security

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const port = 5000
 const mongoose = require('mongoose')
 const { User } = require('./models/User')
 const bodyParser = require('body-parser')
+const config = require('./config/key');
 
 //application/x-www-form-urlencodedタイプのデータ分析のoption
 app.use(bodyParser.urlencoded({extended:true}));
 //application/jsonタイプのデータ分析のoption
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://blkctn:bc12345@bt0-6nhkl.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(() => console.log("mongoDB connected..."))
   .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Change World!'))
 
 app.post('/register', (req,res) => {
   const user = new User(req.body)
