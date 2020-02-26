2898bea23512689780753e770dec4b70b4ec1e8f
shcoffee <sheepbrandy@gmail.com>
2020-02-27 00:54:46 +0900

    .

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
-const { User } = require('./models/User')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const config = require('./config/key');
+const { auth } = require('./middleware/auth')
+const { User } = require('./models/User')
 
 //application/x-www-form-urlencodedタイプのデータ分析のoption
 app.use(bodyParser.urlencoded({extended:true}));
 //application/jsonタイプのデータ分析のoption
 app.use(bodyParser.json());
+
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI, {
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
@@ -19,7 +23,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello Change World!'))
 
-app.post('/register', (req,res) => {
+app.post('/api/user/register', (req,res) => {
   const user = new User(req.body)
   //userモーデルにデータ保存
   user.save((err, userInfo) => {
@@ -28,6 +32,53 @@ app.post('/register', (req,res) => {
       success: true
     })
   })
+})
+
+app.post('/api/user/login', (req,res) => {
+  User.findOne({email : req.body.email}, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess : false,
+        message : 'このメールアドレスではIDが登録されていません。'
+      })
+    }
+    console.log('req.body.password:'+req.body.password);
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      console.log('isMatch2:'+isMatch);
+      if(!isMatch) 
+        return res.json({loginSuccess:false, message:'パスワードが正しくありません。'})
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err)
+          res.cookie('x_auth', user.token)
+          .status(200)
+          .json({loginSuccess:true, userId: user._id})
+      })
+    })
+  })
+})
+
+app.get('/api/user/auth', auth, (req, res) => {
+  res.status(200).json({
+    _id : req.user._id,
+    isAdmin : req.user.role === 0 ? false : true,
+    isAuth : true,
+    email : req.user.email,
+    name : req.user.name,
+    lastname : req.user.lastname,
+    role : req.user.role,
+    image : req.user.image
+  })
+})
+
+app.get('/api/user/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id : req.user._id},
+    {token : ""},
+    (err, user) => {
+      if (err) return res.json({success : false, err});
+      return res.status(200).send({
+        success : true
+      })
+    })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jsonwebtoken = require('jsonwebtoken')
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name : {
@@ -30,6 +33,51 @@ const userSchema = mongoose.Schema({
         type : Number
     }
 })
+
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+
+    console.log('isMatch:'+isMatch);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.method.generateWebToken = function(cb) {
+    var user = this;
+    user.token = jsonwebtoken.sign(user._id.toHexString(), 'token1');
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+    jsonwebtoken.verify(token, 'token1', function(err, decoded) {
+        user.findOne({"id" : decoded, "token" : token}, function(err, user) {
+            if (err) return cb (err);
+            cb(null, user);
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
